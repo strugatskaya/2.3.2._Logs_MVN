1948e60bb75f77f9a96383a4c6be02d89e3b6b5e
Juliya Strugatskaya <strugatskaya.um@gmail.com>
2021-06-01 00:42:03 +0300

    fixed #1

--- bonus-service/src/main/java/Main.java
+++ bonus-service/src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
   public static void main(String[] args) {
     BonusService service = new BonusService();
 
-    long amount = 1002_60;
+    long amount = 1000_60;
     boolean registered = true;
 
     service.calculate(amount, registered);
